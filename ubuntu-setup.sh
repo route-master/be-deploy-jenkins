b77beba73692848b0335042e7c297e338181e6bd
Taeuk Um <42173347+umtuk@users.noreply.github.com>
2023-03-25 16:23:11 +0900

    Create ubuntu-setup.sh

---
+++ ubuntu-setup.sh
@@ -0,0 +1,15 @@
+sudo apt-get update
+sudo apt install curl -y
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install fontconfig openjdk-11-jre -y
+sudo apt-get install jenkins -y
+
+sudo service jenkins start
